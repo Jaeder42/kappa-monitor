f98148426a17213b0738017e765ad0d86bd99695
jaeder42 <jaeder42@gmail.com>
2018-03-02 00:09:53 +0100

    deployment

---
+++ bot.js
@@ -0,0 +1,35 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('pong');
+  }
+  if (msg.content === 'PÖMPS!') {
+    msg.reply('What do you mean?!');
+  }
+  if (msg.content === 'Make me a scrub') {
+    try {
+      msg.member.addRole('380358333862772737');
+      msg.reply('Yes sir');
+    } catch (err) {
+      console.log(err);
+    }
+  }
+});
+
+client.on('guildMemberAdd', member => {
+  // Send the message to a designated channel on a server:
+  var channel = member.guild.channels.find('name', 'member-log');
+  // Do nothing if the channel wasn't found on this server
+  if (!channel) return;
+  // Send the message, mentioning the member
+  msg.member.addRole('380358333862772737');
+  channel.send(`Welcome to the server, ${member}`);
+});
+
+client.login('NDE4Nzk1NTIwMTQ4ODk3Nzkz.DXoVUA.S6ddNsdxKTE33dpJo54rsZ08Hsk');
