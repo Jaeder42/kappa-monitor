f9886fc370edeebf4e1d0e32230bff69d568db4a
jaeder42 <jaeder42@gmail.com>
2018-03-02 20:06:31 +0100

    Bot now repsonds only if pinged

--- src/handlers/message/index.ts
+++ src/handlers/message/index.ts
@@ -6,16 +6,20 @@ const message = async (msg) => {
       mentions.forEach(async (mention: any) => {
         const user = mention[1].user;
         if (user.username === 'KappaMonitor' && user.discriminator === '9633') {
-          await msg.reply('It me!');
+          if (msg.content === 'ping') {
+            await msg.reply('pong');
+          }
+          else if (msg.content === 'PÖMPS!') {
+            await msg.reply('What do you mean?!');
+          } else if (msg.content === 'help') {
+            await msg.reply('no help here min vännen');
+          } else {
+            await msg.reply('It me!');
+          }
         }
       });
     }
-    if (msg.content === 'ping') {
-      await msg.reply('pong');
-    }
-    if (msg.content === 'PÖMPS!') {
-      await msg.reply('What do you mean?!');
-    }
+
   } catch (err) {
     log.error(err);
   }
