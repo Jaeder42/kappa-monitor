befcfc5cef0234c5d0cefab619fa56fa4da8228d
Johan Jäderblom <johan.jaderblom@valtech.se>
2018-03-02 09:59:57 +0100

    fixed cron job

--- src/cron.ts
+++ src/cron.ts
@@ -1,9 +1,9 @@
 // Hacky workaround for Heroku idling free apps.
 
 import * as cron from 'node-cron';
-
 export default (cb) => {
-  const cronJob = cron.cronJob('* */10 * * * *', () =>{
+
+  const cronJob = cron.schedule('* */10 * * * *', () => {
     cb();
   }, null, true);
 }
